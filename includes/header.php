7f9740882b29921910d87c3f372def6ee69d990f
Dobos Cosmin <dobos_cosmin86@yahoo.com>
2024-11-06 00:59:01 +0200

    add nav

--- includes/header.php
+++ includes/header.php
@@ -17,17 +17,72 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<header>
     <div class="container">
-        <a class="navbar-brand" href="index.php">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php">Acasă</a></li>
-
-            </ul>
+        <div class="header-up d-none d-md-block">
+            <div class="header-up__wrapper d-flex justify-content-between ">
+                <div class="header-up__socials d-flex align-items-center ">
+                        <a class="social" href="#" target="_blank">
+                            <img class="social__image" src="./assets/icons/facebook.svg"  width="12" height="23">
+                        </a>
+                        <a class="social" href="#" target="_blank">
+                            <img class="social__image" src="./assets/icons/twitter.svg"  width="22" height="22">
+                        </a>
+                </div>
+                <div class="header-up__nav">
+                        <ul class="nav__menu d-inline-flex ">
+                            <li class="nav__item list__item">Blog</li>
+                            <li class="nav__item list__item">Despre noi</li>
+                            <li class="nav__item list__item">Solicita oferta</li>
+                            <li class="nav__item list__item">EUR</li>
+                            <li class="nav__item list__item">Steag</li>
+                        </ul>
+                </div>
+            </div>
         </div>
+
+        <div class="header-middle">
+            <div class="header-middle__wrapper d-flex justify-content-between">
+                <div class="header-middle__logo">
+                        <a class="logo__anchor" href="/">
+                            <img class="logo__image" src="./assets/img/logo-direct.png" alt="">
+                        </a>
+                </div>
+                <div class="header-middle__contact d-md-flex d-none  ">
+                    <div class="contact__account  d-flex align-items-center" >
+                        <img class="contact__image" src="./assets/icons/account-image.svg" width="33" height="33">
+                        <p class="contact__text">Contul meu</p>
+                        <img  class="contact__arrow" src="./assets/icons/arrow-up.svg"  width="14" height="13">
+                    </div>
+                    <div class="contact__details">
+                        <div class="contact__phone">
+                            <img class="phone__image" src="./assets/icons/smartphone-call.svg"  width="24" height="24">
+                            <span class="phone__number">031 9641</span>
+                        </div>
+                        <div class="contact__shcedule">
+                            <span class="schedule__details">Luni-Vineri 9-18</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact__hamburger d-md-none">
+                    <img  class="contact__image" src="./assets/icons/menu-hamburger.svg"  width="30" height="30">
+                </div>
+            </div>
+        </div>
+
+        <div class="header-bottom d-none d-md-block">
+            <div class="header-bottom__wrapper">
+                <ul class="nav__menu d-flex">
+                    <li class="nav__item list__item">Vacanțe</li>
+                    <li class="nav__item list__item">Destinații</li>
+                    <li class="nav__item list__item">Obiective turistice</li>
+                    <li class="nav__item list__item">Utile</li>
+                    <li class="nav__item list__item">Testimoniale</li>
+                    <li class="nav__item list__item">Contact</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="header-mobile"></div>
     </div>
-</nav>
+</header>
